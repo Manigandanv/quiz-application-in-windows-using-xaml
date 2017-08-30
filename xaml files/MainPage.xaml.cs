04199e37564273c0eb25709fb87d9d992a67509c
Manigandanv <manigandanv2012@vit.ac.in>
2017-08-30 16:50:04 +0530

    Add files via upload

---
+++ xaml files/MainPage.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Basic Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234237
+
+namespace quizapp
+{
+    /// <summary>
+    /// A basic page that provides characteristics common to most applications.
+    /// </summary>
+    public sealed partial class MainPage : quizapp.Common.LayoutAwarePage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Storyboard1.Begin();
+            Storyboard2.Begin();
+        }
+
+        /// <summary>
+        /// Populates the page with content passed during navigation.  Any saved state is also
+        /// provided when recreating a page from a prior session.
+        /// </summary>
+        /// <param name="navigationParameter">The parameter value passed to
+        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested.
+        /// </param>
+        /// <param name="pageState">A dictionary of state preserved by this page during an earlier
+        /// session.  This will be null the first time a page is visited.</param>
+        protected override void LoadState(Object navigationParameter, Dictionary<String, Object> pageState)
+        {
+        }
+
+        /// <summary>
+        /// Preserves state associated with this page in case the application is suspended or the
+        /// page is discarded from the navigation cache.  Values must conform to the serialization
+        /// requirements of <see cref="SuspensionManager.SessionState"/>.
+        /// </summary>
+        /// <param name="pageState">An empty dictionary to be populated with serializable state.</param>
+        protected override void SaveState(Dictionary<String, Object> pageState)
+        {
+        }
+        private void JAVA_clicked(object sender, RoutedEventArgs e)
+        {
+            if (this.Frame != null)
+            {
+                this.Frame.Navigate(typeof(nextPage1));
+            }
+
+        }
+
+        private void CSHARP_clicked(object sender, RoutedEventArgs e)
+        {
+            if (this.Frame != null)
+            {
+                this.Frame.Navigate(typeof(nextpage));
+            }
+        }
+
+        private void C_clicked(object sender, RoutedEventArgs e)
+        {
+            if (this.Frame != null)
+            {
+                this.Frame.Navigate(typeof(nextPage2));
+            }
+        }
+
+        private void CPLUSPLUS_clicked(object sender, RoutedEventArgs e)
+        {
+            if (this.Frame != null)
+            {
+                this.Frame.Navigate(typeof(nextPage3));
+            }
+        }
+
+        private void PHP_clicked(object sender, RoutedEventArgs e)
+        {
+            if (this.Frame != null)
+            {
+                this.Frame.Navigate(typeof(nextPage4));
+            }
+        }
+
+       // private void Button_Click_1(object sender, RoutedEventArgs e)
+        //{
+          //  if (radioButton1.IsChecked == true)
+            //{
+              //  answer.Text = "correct";
+            //}
+            //else if (radioButton8.IsChecked == true)
+            //{
+              //  answer1.Text = "correct";
+            //}
+            //else if (rb2.IsChecked == true)
+            //{
+              //  answer2.Text = "correct";
+            //}
+            //else
+              //  answer1.Text = "wrong";
+        //}
+
+    }   
+}
